c0e16754fbb9288889bb80da90999444e570d31a
Samuel Andres <andresro_9219@hotmail.com>
2019-03-27 22:12:22 -0500

    Pruebas

--- test/Ci2HRodriguez.Test/Servicios/LaTarea.cs
+++ test/Ci2HRodriguez.Test/Servicios/LaTarea.cs
@@ -20,6 +20,9 @@ namespace Ci2HRodriguez.Test.Servicios
         /// </summary>
         private Mock<ITareaRepositorio> _TareaRepositorio;
 
+        /// <summary>
+        /// Mock de autoMapper
+        /// </summary>
         private Mock<IMapper> _Mapper;
 
         /// <summary>
@@ -63,6 +66,50 @@ namespace Ci2HRodriguez.Test.Servicios
             // Assert
             Assert.NotNull(resultado);
             _TareaRepositorio.Verify(c => c.AgregarTareaAsync(It.IsAny<Tarea>()), Times.Once);
+        }
+
+        [Fact]
+        public async void DevuelveUnaTareaActualizada()
+        {
+            // Arange
+            var tarea = new Tarea();
+            _TareaRepositorio.Setup(c => c.ActualizarTareaAsync(It.IsAny<Tarea>())).ReturnsAsync(tarea);
+
+            // Act
+            var resultado = await _TareaServicio.ActualizarTareaAsync(It.Is<Guid>(guid => guid != Guid.Empty),
+                                                                      It.IsAny<string>(), 
+                                                                      It.IsAny<string>(), 
+                                                                      It.IsAny<DateTime>(),
+                                                                      It.IsAny<bool>());
+
+            // Assert
+            Assert.NotNull(resultado);
+            _TareaRepositorio.Verify(c => c.ActualizarTareaAsync(It.IsAny<Tarea>()), Times.Once);
+        }
+
+        [Fact]
+        public async void BorraRegistroDadoElIdDeLaTarea()
+        {
+            // Arange
+            _TareaRepositorio.Setup(c => c.ObtenerTareaPorIdAsync(It.IsAny<Guid>())).ReturnsAsync(new Tarea());
+            _TareaRepositorio.Setup(c => c.BorrarTareaAsync(It.Is<Guid>(guid => guid != Guid.Empty)));
+
+            // Act
+            await _TareaServicio.BorrarTareaAsync(Guid.NewGuid(), It.IsAny<string>());
+
+            // Assert
+            _TareaRepositorio.Verify(c => c.BorrarTareaAsync(It.IsAny<Guid>()), Times.Once);
+        }
+
+        [Fact]
+        public async void DevuelveExcepcionDadoQueElUsuarioNoEsResponsableDeLaTarea()
+        {
+            // Arrange
+            _TareaRepositorio.Setup(c => c.ObtenerTareaPorIdAsync(It.IsAny<Guid>())).ReturnsAsync(new Tarea());
+            _TareaRepositorio.Setup(c => c.BorrarTareaAsync(It.Is<Guid>(p => p != Guid.Empty))).Throws<ArgumentNullException>();
+
+            // Act && Assert
+            await Assert.ThrowsAnyAsync<Exception>(async () => await _TareaServicio.BorrarTareaAsync(Guid.NewGuid(), Guid.NewGuid().ToString()));
         }
     }
 }
